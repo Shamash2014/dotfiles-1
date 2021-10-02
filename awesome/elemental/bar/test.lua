cf4e0db1c21ca84a28bab68cb77c11cdc413c725
aidanhopper <aidanhop1@gmail.com>
2021-10-01 20:57:49 -0700

    wow thats a lot of stuff

--- awesome/elemental/bar/test.lua
+++ awesome/elemental/bar/test.lua
@@ -1,91 +1,168 @@
-local awful = require("awful")
-local gears = require("gears")
-local wibox = require("wibox")
+local awful     = require("awful")
+local gears     = require("gears")
+local wibox     = require("wibox")
 local beautiful = require("beautiful")
-local keys = require("utils.keybinds")
-local dpi = require("beautiful.xresources").apply_dpi
+local keys      = require("utils.keybinds")
+local dpi       = require("beautiful.xresources").apply_dpi
+local helper    = require("utils.helpers")
+local naughty   = require("naughty")
 
-
-beautiful.taglist_text_color_focused  = "#B48EAD"
-beautiful.taglist_text_color_urgent   = "#B48EAD"
-beautiful.taglist_text_color_occupied = "#B48EAD"
-beautiful.taglist_text_color_empty    = "#B48EAD"
-
-local update_taglist = function (item, tag, index)
-    if tag.selected then
-        item.bg = beautiful.taglist_text_color_focused
-    elseif tag.urgent then
-        item.bg = beautiful.taglist_text_color_urgent
-    elseif #tag:clients() > 0 then
-        item.bg = beautiful.taglist_text_color_occupied
-    else
-        item.bg = beautiful.taglist_text_color_empty
-    end
-end
 
 awful.screen.connect_for_each_screen(function(s)
-
-      -- -- Each screen has its own tag table.
-      -- awful.tag({ " ⭘ ", " ⭘ " }, s, awful.layout.layouts[1])
-
-      -- -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-      -- -- We need one layoutbox per screen.
-      -- s.mylayoutbox = awful.widget.layoutbox(s)
-      -- s.mylayoutbox:buttons(gears.table.join(
-      -- 			       awful.button({ }, 1, function () awful.layout.inc( 1) end),
-      -- 			       awful.button({ }, 3, function () awful.layout.inc(-1) end),
-      -- 			       awful.button({ }, 4, function () awful.layout.inc( 1) end),
-      -- 			       awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-      -- -- Create a taglist widget
-      -- s.mytaglist = awful.widget.taglist {
-      -- 	 screen  = s,
-      -- 	 filter  = awful.widget.taglist.filter.all,
-      -- 	 buttons = keys.taglist_buttons,
-      -- 	 layout = wibox.layout.flex.horizontal,
-      -- }
+      -- Each screen has its own tag table.
+      awful.tag({ " ⭘ ", " ⭘ ", " ⭘ " }, s, awful.layout.layouts[1])
+      awful.tag.font = "VictorMono Nerd Font Mono 24"
       
-      -- -- Create the wibox
-      -- s.taglist_box = awful.wibar({
-      -- 	    position = "bottom",
-      -- 	    screen = s,
-      -- 	    height = dpi(38),
-      -- 	    width = dpi(2500),
-      -- 	    opacity = .7,
-      -- 	    bg = "#4C566A"
-      -- })
+      s.clock = wibox.widget.textclock()
 
-      -- Add widgets to the wibox
-      -- s.taglist_box:setup {
-      -- 	 layout = wibox.layout.align.horizontal,
-      -- 	 { -- Left widgets
-      --       layout = wibox.layout.fixed.horizontal,
+      -- Create a promptbox for each screen
+      -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+      -- We need one layoutbox per screen.
+      s.mylayoutbox = awful.widget.layoutbox(s)
+      s.mylayoutbox:buttons(gears.table.join(
+			       awful.button({ }, 1, function () awful.layout.inc( 1) end),
+			       awful.button({ }, 3, function () awful.layout.inc(-1) end),
+			       awful.button({ }, 4, function () awful.layout.inc( 1) end),
+			       awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+      -- Create a taglist widget
+      s.mytaglist = awful.widget.taglist {
+	 screen  = s,
+	 filter  = awful.widget.taglist.filter.all,
+	 buttons = keys.taglist_buttons,
+      }
+
+      -- Create a tasklist widget
+      s.mytasklist = awful.widget.tasklist {
+	 screen  = s,
+	 filter  = awful.widget.tasklist.filter.currenttags,
+	 buttons = keys.tasklist_buttons,
+	 style    = {
+	    shape  = gears.shape.rounded_bar,
+	 },
+	 layout   = {
+	    spacing = 20,
+	    layout  = wibox.layout.fixed.horizontal
+	 },
+	 widget_template = {
+	    {
+	       {
+		  {
+		     {
+			id     = 'clienticon',
+			widget = awful.widget.clienticon,
+		     },
+		     margins = 5,
+		     widget  = wibox.container.margin,
+		  },
+		  bg = "#4C566A",
+		  shape = gears.shape.circle,
+		  widget = wibox.container.background,
+	       },
+	       margins = 8,
+	       widget = wibox.container.margin,
+	    },
+	    nil,
+	    create_callback = function(self, c, index, objects) --luacheck: no unused args
+	       self:get_children_by_id('clienticon')[1].client = c
+	    end,
+	    layout = wibox.layout.align.vertical,	    
+	 },
+      }
+      
+
+      -- spacer widget
+      local spcr = wibox.widget.textbox("   ")
+      
+      local icons   = wibox.widget.systray()      
+      -- local systray = wibox.widget{
+      -- 	 {
+      -- 	    icons,
+      -- 	    layout = wibox.layout.fixed.horizontal,
       -- 	 },
-      -- 	 { -- Right widgets
-      --       layout = wibox.layout.fixed.horizontal,
-      -- 	    s.mytaglist,
-      -- 	    s.mylayoutbox
-      -- 	 },
+      -- 	 margins = 10,
+      -- 	 widget  = wibox.container.margin,
       -- }
 
-      s.systray = wibox.widget.systray()
-      s.traybox = wibox({
-	    screen = s,
-	    width = dpi(150),
-	    height = dpi(38),
-	    opacity = .7,
-      })
+      local systray = helper.circle_widget(
+	    "", -- bg
+	    "", -- border
+	    0,         -- border size
+	    0,         -- inside margin
+	    8,         -- outside margin
+	    icons     -- widget
+      )
+       
+      beautiful.systray_icon_spacing = 20
+      beautiful.bg_systray = "#2E3440"
+      --beautiful.bg_systray = gears.color.transparent
 
-      s.traybox:setup{
+      
+      -- Create the wibox
+      s.bar = wibox {
+	 screen  = s,
+	 visible = true,
+	 type    = "dock",
+	 width   = awful.screen.focused().geometry.width - dpi(80),
+	 height  = dpi(50),
+	 y       = dpi(20),
+	 x       = dpi(40),
+	 bg      = gears.color.transparent,
+	 opacity = .85
+      }
+
+      local bigsep = wibox.widget {
+	 visible    = false,
+	 span_ratio = 0.1,
+	 widget = wibox.widget.separator
+      }
+
+      
+      s.bar:setup {
 	 {
 	    {
-	       s.systray,
-	       expand = "none",
-	       layoyt = wibox.layout.align.horizontal,
+	       {
+		  -- left
+		  {
+		     spcr,
+		     s.mytaglist,
+		     layout = wibox.layout.fixed.horizontal,
+		     
+		  },
+		  -- center
+		  layout = wibox.layout.fixed.horizontal,
+		  s.mytasklist,
+		  
+		  -- right
+		  {
+		     layout = wibox.layout.fixed.horizontal,
+		     --icons,
+		     systray,
+		     spcr,
+		     s.mylayoutbox,
+		     spcr,
+		     s.clock,
+		     spcr,
+		  },
+		  expand = "none",
+		  layout = wibox.layout.align.horizontal
+	       },
+	       bg      = "#2E3440",
+	       --shape  = gears.shape.rounded_rect,
+	       widget  = wibox.container.background,
 	    },
-	    margins = dpi(10),
-	    widget = wibox.container.margin,
+	    margins = 7,
+	    widget  = wibox.container.margin,
 	 },
-	 widget = wibox.container.background
+	 bg     = "#4C566A",
+	 --shape  = gears.shape.rounded_rect,
+	 widget = wibox.container.background,
       }
+      s.bar:struts {
+	 --top = dpi(70),
+	 top = dpi(s.bar.height - beautiful.useless_gap + 10),
+      }
+      
+      
+
 end)
 
